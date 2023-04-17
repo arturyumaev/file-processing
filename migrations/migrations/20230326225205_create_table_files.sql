e4e687f2980795526789b0f914ebf6839f059aac
Artur Yumaev <art.yumaev@gmail.com>
2023-04-18 00:02:36 +0300

    Removed trigger on table

--- migrations/migrations/20230326225205_create_table_files.sql
+++ migrations/migrations/20230326225205_create_table_files.sql
@@ -11,31 +11,10 @@ create table files (
 
 create index files_filename_idx on files(filename);
 create index files_timestamp_idx on files(timestamp);
-
--- trigger function
-create or replace function trigger_function_valid_status_type()
-  returns trigger
-  language plpgsql
-as $$
-begin
-	if new.status not in ('recieved', 'in_queue', 'processing', 'done', 'error') then
-    raise exception 'wrong status type';
-  end if;
-
-	return new;
-end $$;
-
--- trigger
-create trigger trigger_valid_status_type
-  before insert or update
-  on files
-  for each row execute function trigger_function_valid_status_type();
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-drop trigger trigger_valid_status_type on files;
-drop function trigger_function_valid_status_type;
 drop index files_filename_idx;
 drop index files_timestamp_idx;
 drop table files;
